9a28a3d9f1e6aab6144528431d4436c98bb9c0ab
pparksso <thgml5143@hanmail.net>
2022-08-16 07:29:23 +0900

    weather api 정리

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
+const body = document.querySelector("body");
 const city = document.querySelector("#header .city");
 const hourBox = document.querySelector("#header .hour");
 const minBox = document.querySelector("#header .min");
-let region = "";
-let tempRegion = "";
 
+let tempRegion = "";
+let cityName = "";
 //시간
 const today = () => {
   const hour = new Date().getHours();
@@ -14,50 +15,46 @@ const today = () => {
   minBox.innerHTML = resultMin;
 };
 
-// 처음 로딩됐을 때 현재 위치 지역 이름 찾기
+const backgroundApi = (cityName) => {
+  axios({
+    url: `https://api.unsplash.com/photos/?client_id=${unsplashKey}&query=${cityName}`,
+  })
+    .then((res) => {
+      const backImg = res.data[0].urls.regular;
+      body.style.backgroundImage = `url('${backImg}')`;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+//현재 위치(위도,경도)로 날씨를 구하고, 언어감지를 통해 한국어로 번역
 const currentLocation = navigator.geolocation.getCurrentPosition(async (position) => {
   const latitude = position.coords.latitude;
   const longitude = position.coords.longitude;
   await axios({
-    url: `https://dapi.kakao.com/v2/local/geo/coord2regioncode.json?x=${longitude}&y=${latitude}`,
-    method: "GET",
-    headers: {
-      Authorization: `KakaoAK ${kakaoKey}`,
-    },
+    url: `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${openWeatherKey}&lang=kr&units=metric`,
   })
     .then((res) => {
-      const data = res.data.documents[1];
-      region = data.address_name;
-      city.innerHTML = region;
+      console.log(res.data);
+      const data = res.data;
+      cityName = data.name;
+      backgroundApi();
+    })
+    .then(() => {
+      axios({
+        method: "POST",
+        url: "http://127.0.0.1:8099/detectlang",
+        data: {
+          cityName,
+        },
+      }).then((response) => {
+        const translate = response.data.message.result.translatedText;
+        city.innerHTML = translate;
+      });
     })
     .catch((err) => {
       console.log(err);
     });
 });
 
-axios({
-  method: "POST",
-  url: "http://127.0.0.1:8099/weatherlang",
-  data: {
-    region: "부산",
-  },
-})
-  .then((res) => {
-    console.log(res);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-const backgroundApi = async () => {
-  await axios({
-    url: `https://api.unsplash.com/photos/?client_id=${unsplashKey}&query=${region}`,
-  })
-    .then((res) => {
-      console.log(res.data);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-};
-// backgroundApi();
 today();
