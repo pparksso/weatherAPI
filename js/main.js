e68ec7ef2fb2e26a5b1495ba63ed0ecc06c05580
pparksso <thgml5143@hanmail.net>
2022-08-18 07:51:50 +0900

    UI 수정중

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 const body = document.querySelector("body");
 const city = document.querySelector("#header .city");
+const amPmBox = document.querySelector("#header .amPm");
 const hourBox = document.querySelector("#header .hour");
 const minBox = document.querySelector("#header .min");
-const nowTemper = document.querySelector("#top .nowTemper");
 const todayWeatherList = document.querySelector("#top .todayList ul");
-const ctx = document.querySelector("#myChart").getContext("2d");
+// const ctx = document.querySelector("#myChart").getContext("2d");
 let tempRegion = "";
 let cityName = "";
 //시간
@@ -13,34 +13,33 @@ const today = () => {
   const min = new Date().getMinutes();
   const resultHour = hour < 10 ? `0${hour}` : hour;
   const resultMin = min < 10 ? `0${min}` : min;
+  const amPm = hour < 11 ? "AM" : "PM";
   hourBox.innerHTML = resultHour;
   minBox.innerHTML = resultMin;
+  amPmBox.innerHTML = amPm;
 };
-// 백그라운드
-const backgroundApi = (cityName) => {
-  axios({
-    url: `https://api.unsplash.com/photos/?client_id=${unsplashKey}&query=${cityName}`,
-  })
-    .then((res) => {
-      const backImg = res.data[0].urls.regular;
-      body.style.backgroundImage = `url('${backImg}')`;
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-};
+
 //현재 위치(위도,경도)로 날씨를 구하고, 언어감지를 통해 한국어로 번역
 const currentLocation = navigator.geolocation.getCurrentPosition(async (position) => {
   const latitude = position.coords.latitude;
   const longitude = position.coords.longitude;
   await axios({
+    url: `https://dapi.kakao.com/v2/local/geo/coord2regioncode.json?x=${longitude}&y=${latitude}`,
+    headers: {
+      Authorization: `KakaoAK ${kakaoKey}`,
+    },
+  }).then((res) => {
+    const region01 = res.data.documents[0].region_1depth_name;
+    const region02 = res.data.documents[0].region_2depth_name;
+    city.innerHTML = `${region01} ${region02}`;
+  });
+  await axios({
     url: `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${openWeatherKey}&units=metric`,
   })
     .then((res) => {
       const data = res.data;
+      console.log(data);
       cityName = data.name;
-      backgroundApi(cityName);
-      nowTemper.innerHTML = `${Math.floor(data.main.temp)}`;
       todayWeatherList.innerHTML = `
       <li class="today">
                 <span class="">Today</span>
@@ -50,16 +49,16 @@ const currentLocation = navigator.geolocation.getCurrentPosition(async (position
       `;
     })
     .then(() => {
-      axios({
-        method: "POST",
-        url: "http://127.0.0.1:8099/detectlang",
-        data: {
-          cityName,
-        },
-      }).then((response) => {
-        const translate = response.data.message.result.translatedText;
-        city.innerHTML = translate;
-      });
+      // axios({
+      //   method: "POST",
+      //   url: "http://127.0.0.1:8099/detectlang",
+      //   data: {
+      //     cityName,
+      //   },
+      // }).then((response) => {
+      //   const translate = response.data.message.result.translatedText;
+      //   city.innerHTML = translate;
+      // });
     })
     .catch((err) => {
       console.log(err);
@@ -69,75 +68,33 @@ const currentLocation = navigator.geolocation.getCurrentPosition(async (position
   })
     .then((res) => {
       const list = res.data.list;
-      let tempArr = [];
-      for (let i = 0; i < list.length; i++) {
-        tempArr.push(Math.floor(list[i].main.temp));
-      }
-      const mixedChart = new Chart(ctx, {
-        data: {
-          datasets: [
-            {
-              type: "bar",
-              label: "Bar Dataset",
-              data: tempArr,
-              backgroundColor: "rgba(255,255,255,0.3)",
-              order: 2,
-            },
-            {
-              type: "line",
-              label: "Line Dataset",
-              data: tempArr,
-              fill: false,
-              borderColor: "#fff",
-              order: 1,
-            },
-          ],
-          labels: ["12PM", "3AM", "6AM", "9AM", "12AM", "3PM", "6PM", "9PM"],
-        },
-        options: {
-          tooltips: {
-            enabled: false,
-          },
-          hover: {
-            animationDuration: 0,
-          },
-          scales: {
-            xAxes: [
-              {
-                ticks: {
-                  fontSize: 11, //x축 텍스트 폰트 사이즈
-                  fontColor: "rgba(0,0,0,0)", //x축 레이브 안보이게
-                },
-                gridLines: {
-                  display: false,
-                  lineWidth: 0,
-                },
-              },
-            ],
-            yAxes: [
-              {
-                ticks: {
-                  display: false, //y축 텍스트 삭제
-                  beginAtZero: false, //y축값이 0부터 시작
-                  display: false,
-                },
-                gridLines: {
-                  display: false,
-                  lineWidth: 0,
-                },
-              },
-            ],
-          },
-          legend: {
-            display: true,
-            labels: {
-              boxWidth: 0,
-              fontColor: "rgba(0,0,0,0)",
-              fontSize: 20,
-            },
-          },
-        },
-      });
+      // let tempArr = [];
+      // for (let i = 0; i < list.length; i++) {
+      //   tempArr.push(Math.floor(list[i].main.temp));
+      // }
+      // const mixedChart = new Chart(ctx, {
+      //   data: {
+      //     datasets: [
+      //       {
+      //         type: "bar",
+      //         label: "Bar Dataset",
+      //         data: tempArr,
+      //         backgroundColor: "rgba(255,255,255,0.3)",
+      //         order: 2,
+      //       },
+      //       {
+      //         type: "line",
+      //         label: "Line Dataset",
+      //         data: tempArr,
+      //         fill: false,
+      //         borderColor: "#fff",
+      //         order: 1,
+      //       },
+      //     ],
+      //     labels: ["12PM", "3AM", "6AM", "9AM", "12AM", "3PM", "6PM", "9PM"],
+      //   },
+      //   options: {},
+      // });
     })
     .catch((err) => {
       console.log(err);
